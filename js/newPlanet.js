2a6be48d61bcee98bd4117b0df27d5c23de41465
Lindsey Hodel <lindsey.c.hodel.19@dartmouth.edu>
2018-11-17 18:27:01 -0500

    deleted guiand js new plan

--- js/newPlanet.js
+++
@@ -1,5 +0,0 @@
-$('input:radio[name="radius"]').change(function () {
-    $('input:radio[name="radius"]:checked').parent().removeClass("q1label unselected").addClass("selected");
-    $('input:radio[name="radius"]').not(':checked').parent().removeClass("q1label selected").addClass("unselected");
-  
-  });
